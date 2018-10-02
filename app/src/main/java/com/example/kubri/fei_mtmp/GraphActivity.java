1995b6292bfbc3a94b1687a4916effe24e2a55db
Juraj Kubričan <kubrican.juraj@gmail.com>
2018-10-02 17:04:27 +0200

    graph

--- app/src/main/java/com/example/kubri/fei_mtmp/GraphActivity.java
+++ app/src/main/java/com/example/kubri/fei_mtmp/GraphActivity.java
@@ -3,7 +3,12 @@ package com.example.kubri.fei_mtmp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 import java.util.ArrayList;
+
 
 public class GraphActivity extends AppCompatActivity {
 
@@ -12,9 +17,19 @@ public class GraphActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
 
-        Preview preview = findViewById(R.id.graphPreview);
         ArrayList<ParabolaPoint> parabolaData = (ArrayList<ParabolaPoint>) getIntent().getExtras().get(MainActivity.PARABOLA_DATA);
-        preview.reDraw(parabolaData);
+
+
+        assert parabolaData != null;
+
+        DataPoint[] dataPoints = new DataPoint[parabolaData.size()];
+        for (int i = 0; i < parabolaData.size(); i++) {
+            ParabolaPoint pp = parabolaData.get(i);
+            dataPoints[i] = new DataPoint(pp.t, pp.y);
+        }
+        GraphView graph = findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(dataPoints);
+        graph.addSeries(series);
 
     }
 }
